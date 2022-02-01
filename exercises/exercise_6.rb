c429732635e55376cabdeee75bd2e49a8c5902a6
tofi-124 <tofikahm@yahoo.com>
2022-02-01 16:53:30 +0000

    finial exs

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  { first_name: "Khurram", last_name: "Virani", hourly_rate: 60 },
+  { first_name: "A", last_name: "B", hourly_rate: 50 },
+  { first_name: "C", last_name: "D", hourly_rate: 65 },
+  { first_name: "E", last_name: "F", hourly_rate: 55 },
+])
+
+@store2.employees.create([
+  { first_name: "G", last_name: "H", hourly_rate: 60 },
+  { first_name: "I", last_name: "J", hourly_rate: 50 },
+  { first_name: "K", last_name: "L", hourly_rate: 65 },
+  { first_name: "M", last_name: "N", hourly_rate: 55 },
+])
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+store_name = gets.chomp
+one = Store.create(name: store_name)
+puts one.errors.messages
+two = Store.create(name: store_name, annual_revenue: 0)
+puts two.errors.messages
+Store.create(name: store_name, annual_revenue: 0, mens_apparel:true)
